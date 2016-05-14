b93991370db6b9561264602ba6b704117f305dd5
JP Gorman <j.p.g.gorman@mgail.com>
2016-05-14 13:15:02 +0100

    Update demo to indicate keyframe types

--- demo/index.js
+++ demo/index.js
@@ -21,11 +21,17 @@ const App = React.createClass({
   render() {
     const {inverted} = this.state
     const className = inverted ? "inverted" : ""
+    const text = inverted ? "Using INVERTED keyframes" : "Using ORIGINAL keyframes"
     return (
       <div
         onClick={this.handler}
         className={`foo ${className}`}
-      >Click Me</div>
+      >
+        Click Me
+        <div className="label">
+        {text}
+        </div>
+      </div>
     )
   },
 
